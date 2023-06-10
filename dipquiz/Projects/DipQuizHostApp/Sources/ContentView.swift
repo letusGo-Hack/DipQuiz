a8f9cfaef5b9f4982e7eb788412f135751d6872b
zime <zime@mac.com>
2023-06-10 16:49:12 +0900

    Host 데이더 받는 부분 처리

--- dipquiz/Projects/DipQuizHostApp/Sources/ContentView.swift
+++ dipquiz/Projects/DipQuizHostApp/Sources/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftData
 import DQCommon
 
 struct ContentView: View {
+    
+    @EnvironmentObject var modelData: ConnectionManager
+    
     var body: some View {
         NavigationStack {
             NavigationLink {
@@ -25,12 +28,28 @@ struct ContentView: View {
                 Label("시작", systemImage: "flag.checkered")
                     .font(.system(size: 30, weight: .medium))
             }
+            
+            ForEach(modelData.messages) { message in
+                Group{}
+            }
+
+//            ForEach(modelData.messages) { message in
+//              MessageBodyView(message: message)
+//                .onAppear {
+//                  if message == chatConnectionManager.messages.last {
+//                    reader.scrollTo(message.id)
+//                  }
+//                }
+//            }
+            
         }.onAppear(){
             //호스트이름 고정
             ConnectionManager.shared.displayname("Dip Quiz Host")
             ConnectionManager.shared.host()
         }
     }
+    
+    
 }
 
 #Preview {
--- dipquiz/Projects/DipQuizHostApp/Sources/hostApp.swift
+++ dipquiz/Projects/DipQuizHostApp/Sources/hostApp.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 import SwiftData
+import DQCommon
 
 @main
 struct hostApp: App {
-
+    @StateObject private var modelData = ConnectionManager.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
         .modelContainer(for: Item.self)
     }
