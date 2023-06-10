13325f5d639f9f14b9f02e4c7b45d93c577a31fd
zime <zime@mac.com>
2023-06-10 10:59:58 +0900

    Command 모듈 일부 추가.(통신 베이스 작업중)

--- dipquiz/host/share/Connection Managers/ConnectionManager.swift
+++ dipquiz/host/share/Connection Managers/ConnectionManager.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Foundation
 import MultipeerConnectivity
 
@@ -46,7 +14,7 @@ class ConnectionManager: NSObject, ObservableObject {
   private var isHosting = false
 
   func send(_ message: String) {
-    let chatMessage = Command(displayName: myPeerId.displayName, body: message)
+    let chatMessage = Command(displayName: myPeerId.displayName, body: message, question_id: "", question: "", answer: "", value: "", type: .NONE)
     messages.append(chatMessage)
     guard
       let session = session,
@@ -121,7 +89,7 @@ extension ConnectionManager: MCNearbyServiceAdvertiserDelegate {
 extension ConnectionManager: MCSessionDelegate {
   func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
     guard let message = String(data: data, encoding: .utf8) else { return }
-    let chatMessage = Command(displayName: peerID.displayName, body: message)
+      let chatMessage = Command(displayName: peerID.displayName, body: message, question_id: "", question: "", answer: "", value: "", type: .NONE)
     DispatchQueue.main.async {
       self.messages.append(chatMessage)
     }
--- dipquiz/host/share/Models/Command.swift
+++ dipquiz/host/share/Models/Command.swift
@@ -1,42 +1,28 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import UIKit
 
+//통신 종류
+enum CommandType: Decodable, Encodable {
+    case NONE
+    case INT
+    case INT_REQUEST
+    case SCORE
+    case SCORE_WAIT
+    case SCORE_REQUEST
+    case STATUS
+    case STATUS_REQUEST
+}
+
 struct Command: Identifiable, Equatable, Codable {
-  var id = UUID()
+  var id = UUID() //접속 성공시 발생되는 ID
   let displayName: String
   let body: String
   var time = Date()
+    
+    var question_id: String
+    var question: String
+    var answer: String
+    var value: String
+    var type: CommandType
 
   var isUser: Bool {
     return displayName == UIDevice.current.name
