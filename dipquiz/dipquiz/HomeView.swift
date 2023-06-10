4f4d935423bc7d4258808215a3227e00933c439e
Ever <qudrms007@naver.com>
2023-06-10 11:01:14 +0900

    Home View 추가

---
+++ dipquiz/dipquiz/HomeView.swift
@@ -0,0 +1,40 @@
+//
+//  HomeView.swift
+//  dipquiz
+//
+//  Created by Ever on 2023/06/10.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            joinButton
+                .frame(height: 64)
+                .padding(.horizontal, 40)
+            Spacer()
+        }
+        .background(Color.white)
+    }
+    
+    private var joinButton: some View {
+        Button(action: joinButtonTapped) {
+            Text("Join")
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.yellow)
+                .cornerRadius(8)
+        }
+    }
+    
+    private func joinButtonTapped() {
+        print("Join Button Tapped")
+    }
+}
+
+#Preview {
+    HomeView()
+}
